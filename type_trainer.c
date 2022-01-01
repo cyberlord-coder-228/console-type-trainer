6999270e9f2afa749e1d23b33e99128e13998da9
cyberlord-coder-228 <cyberlord2003@gmail.com>
2022-01-01 23:55:05 +0200

    added cringe

--- type_trainer.c
+++ type_trainer.c
@@ -10,7 +10,8 @@ enum
     GREEN_BACKGROUND_NUM = 2
 };
 
-char* LATIN_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+char* LATIN_LETTERS = "abcdefghijklmnopqrstuvwxyz";             // has length 26
+char* LEARNER_SEQUENCE = " fjdksla;ghrueiwoqptyvmc,x.z/bn\'";   // has length 32
 
 
 char get_rand_letter(char* arr, int arr_length)
@@ -19,7 +20,7 @@ char get_rand_letter(char* arr, int arr_length)
 }
 
 float simple_exercise(
-    int amount_of_symbols,  // Determines duration of an exercise
+    int amount_of_symbols,      // Determines duration of an exercise
     char* letters_to_practise,
     int arr_length
 )
@@ -52,7 +53,50 @@ float simple_exercise(
         attroff(COLOR_PAIR(color_num));
     }
         
-    return 1.0 * (amount_of_symbols - mistakes) / amount_of_symbols;
+    return (float)(amount_of_symbols - mistakes) / amount_of_symbols;
+}
+
+void print_how_good_user_was(float correct_ratio, float time_sec)
+{
+    printw("\n\nYour accuracy was %.1f percent, ", correct_ratio*100);
+
+    char* postfix;
+    if (correct_ratio == 0.0)
+    {
+        postfix = "did you even try?";
+    }
+    else if (correct_ratio < 0.2)
+    {
+        postfix = "you can do better!";
+    }
+    else if (correct_ratio < 0.4)
+    {
+        postfix = "keep practicing!";
+    }
+    else if (correct_ratio < 0.6)
+    {
+        postfix = "you are half way here!";
+    }
+    else if (correct_ratio < 0.8)
+    {
+        postfix = "you have done quite good!";
+    }
+    else if (correct_ratio < 1)
+    {
+        postfix = "you have done great!";
+    }
+    else if (correct_ratio == 1.0)
+    {
+        postfix = ". Perfect!";
+    }
+    else
+    {
+        postfix = ". Wait what? How that`s even possible?";
+    }
+
+    printw(postfix);
+    printw("\nIt took you %.1f seconds btw", time_sec);
+    refresh();
 }
 
 int main()
@@ -70,17 +114,23 @@ int main()
     init_pair(RED_BACKGROUND_NUM, COLOR_BLACK, COLOR_RED);
     init_pair(GREEN_BACKGROUND_NUM, COLOR_BLACK, COLOR_GREEN);
 
-    float ratio = simple_exercise(
-        60,     // 60 symbols will be generated until function terminates
-        "jf ",  // User will practice symbols 'j', 'f', and space
-        3       // Length of a symbols array
-    );
-    
-    printw("\n\nYour accuracy was %.1f percent, you've done great!", ratio*100);
-    printw("\n(Type in anything to finish practicing.)");
-    refresh();
+    // Now let`s test the prigram
+    for (int i = 1; i < 16; i++)
+    {
+        clock_t clock_begin = clock();
 
-    getch();                    // Stops program from terminating immediately
+        float ratio = simple_exercise(12, LEARNER_SEQUENCE, i * 2);
+
+        clock_t clock_end = clock();
+        double time_spent_seconds = (double)(clock_end - clock_begin);
+
+        print_how_good_user_was(ratio, time_spent_seconds);
+        printw("\n(Type in anything to continue.)");
+        refresh();
+        getch();
+        clear();
+    }
+
     endwin();                   // Ends ncurses mode
 
     return 0;
