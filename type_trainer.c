533a45c40237ca6f3417703bdd9f98b1566bbaab
cyberlord-coder-228 <cyberlord2003@gmail.com>
2022-01-02 23:57:54 +0200

    updated time measurment

--- type_trainer.c
+++ type_trainer.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
+#include <math.h>
 
 
 enum
@@ -14,6 +15,12 @@ enum
 char* LATIN_LETTERS = "abcdefghijklmnopqrstuvwxyz";             // has length 26
 char* LEARNER_SEQUENCE = " fjdksla;ghrueiwoqptyvmc,x.z/bn";     // has length 31
 
+
+struct timeHolder
+{
+    struct timeval start;
+    struct timeval end;
+};
 
 char get_rand_letter(char* arr, int arr_length)
 {
@@ -57,7 +64,7 @@ float simple_exercise(
     return (float)(amount_of_symbols - mistakes) / amount_of_symbols;
 }
 
-void print_how_good_user_was(float correct_ratio, int lpm)
+void print_how_good_user_was(float correct_ratio, int wpm)
 {
     printw("\n\nYour accuracy was %.1f percent, ", correct_ratio*100);
 
@@ -96,7 +103,7 @@ void print_how_good_user_was(float correct_ratio, int lpm)
     }
 
     printw(postfix);
-    printw("\nYour average speed was %d letters per minute", lpm);
+    printw("\nYour average speed was %d words per minute", wpm);
     refresh();
 }
 
@@ -116,25 +123,26 @@ int main()
     init_pair(GREEN_BACKGROUND_NUM, COLOR_BLACK, COLOR_GREEN);
 
     // Now let`s test the prigram
-    int symobols_in_exercise = 100;
+    int symobols_in_exercise = 47;
     for (int i = 1; i < 16; i++)
     {
-        struct timeval start;
-        struct timeval end;
-
-        gettimeofday(&start, NULL);
+        struct timeHolder timer;
+        
+        gettimeofday(&timer.start, NULL);
         float ratio = simple_exercise(
             symobols_in_exercise,
             LEARNER_SEQUENCE,
             1 + i * 2
         );
-        gettimeofday(&end, NULL);
+        gettimeofday(&timer.end, NULL);
 
-        int time_spent_seconds = end.tv_sec - start.tv_sec;
-        int user_speed_lpm =
-            (int)(60 * (float)(symobols_in_exercise / time_spent_seconds));
+        int time_spent_seconds = timer.end.tv_sec - timer.start.tv_sec;
 
-        print_how_good_user_was(ratio, user_speed_lpm);
+        // wpm = typed_symbols / time (sec) * 60 (sec) / 5 (average word length)
+        int user_speed_wpm =
+            (int)(12 * (float)symobols_in_exercise / (float)time_spent_seconds);
+
+        print_how_good_user_was(ratio, user_speed_wpm);
         printw("\n(Type in anything to continue.)");
         refresh();
         getch();
